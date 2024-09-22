5f79deece43f3c904d0be53186b526766819e924
Prem Tatkari <tatkariprem08@gmail.com>
2024-09-22 21:08:42 +0530

    Extracting path and responding with 200 or 404

--- app/main.js
+++ app/main.js
@@ -6,8 +6,35 @@ console.log("Logs from your program will appear here!");
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
     socket.on('data', (data) => {
-        const response = 'HTTP/1.1 200 OK\r\n\r\n';
-        socket.write(response);
+        const request = data.toString();
+        // console.log("request",request);
+
+
+        // Extract the HTTP method and path from the request
+        const Lines = request.split('\r\n');
+        const requestLine = Lines[0]; // First line contains method, path, and HTTP version
+        const parts = requestLine.split(' '); // Split by spaces
+
+
+        if (parts.length >= 3) {
+            const method = parts[0]; // e.g., GET
+            const path = parts[1]; // e.g., /index.html
+            const httpVersion = parts[2]; // e.g., HTTP/1.1
+
+            // console.log(`Method: ${method}`);
+            // console.log(`Path: ${path}`);
+            // console.log(`HTTP Version: ${httpVersion}`);
+
+            if (path.length > 1) {
+                socket.write('HTTP/1.1 404 Not Found\r\n\r\n')
+            } else {
+                socket.write('HTTP/1.1 200 OK\r\n\r\n')
+            }
+        }
+
+
+        // const response = 'HTTP/1.1 200 OK\r\n\r\n';
+        // socket.write(response);
         socket.end();
     })
 
