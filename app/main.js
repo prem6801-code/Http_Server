b3491517e7ab95316664b9553d252a3cf63d8c49
Prem Tatkari <tatkariprem08@gmail.com>
2024-09-22 20:27:40 +0530

    Server responding to client

--- app/main.js
+++ app/main.js
@@ -5,9 +5,15 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-  socket.on("close", () => {
-    socket.end();
-  });
+    socket.on('data', (data) => {
+        const response = 'HTTP/1.1 200 OK\r\n\r\n';
+        socket.write(response);
+        socket.end();
+    })
+
+    socket.on("close", () => {
+        socket.end();
+    });
 });
 
 server.listen(4221, "localhost");
